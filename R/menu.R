6e515a489be286923f173223f0203c2e1a2b70fd
Gabor Csardi <csardi.gabor@gmail.com>
2015-09-19 21:58:50 +0200

    Menu to go directly to error, support non-primitives as well

--- R/menu.R
+++ R/menu.R
@@ -3,26 +3,46 @@ pipe_menu <- function(dump) {
 
   ## Interactive menu
   pipe_stack <- TRUE
-  titles <- c("\nEnter a frame number, 1 to switch mode, or 0 to exit  ",
-              "\nEnter a pipe stage number, 1 to switch mode, or 0 to exit  ")
-  other_text <- c("Show pipe stages\n", "Show full stack frames\n")
 
-  stacks <- list(dump$calls, dump$chr_stages)
+  stacks <- list(
+    c("   Switch mode\n", dump$calls),
+    c("   Switch mode", "   Take me to the error\n", dump$chr_stages)
+  )
 
   repeat {
 
     which <- menu(
-      c(other_text[pipe_stack + 1], stacks[[pipe_stack + 1]]),
-      title = titles[pipe_stack + 1])
+      stacks[[pipe_stack + 1]],
+      title = "\nEnter 0 to exit or choose:"
+    )
 
     if (which == 1) {
       pipe_stack <- ! pipe_stack
 
     } else if (pipe_stack && which == 2) {
+      if (dump$can_browse) {
+        evalq(browser(), envir = sys.frame(length(dump$calls)))
+      } else {
+        cat("\n")
+        cat(
+          sep = "\n",
+          strwrap(paste(
+            "The error was in a pipe stage that is a primitive function",
+            "that cannot be browsed. This is the pipe stage that called it."
+          ))
+        )
+        cat("\n")
+        evalq(
+          browser(),
+          envir = sys.frame(dump$freduce_calls[dump$bad_stage])
+        )
+      }
+
+    } else if (pipe_stack && which == 3) {
       evalq(browser(), envir = sys.frame(dump$pipe_call))
 
-    } else if (which > 1 && pipe_stack) {
-      which <- which - 2
+    } else if (which > 3 && pipe_stack) {
+      which <- which - 3
       evalq(browser(), envir = sys.frame(dump$freduce_calls[which]))
 
     } else if (which > 1 && ! pipe_stack) {
